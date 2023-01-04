d6f801cc6148937fde7cf858443eaf7a8dc81567
kimmothy <kimmothy@naver.com>
2023-01-04 21:02:16 +0900

    2023-01-04_delete_unsuported_Dem

--- src/main/java/com/maproom/api/domain/Room.java
+++ src/main/java/com/maproom/api/domain/Room.java
@@ -2,20 +2,28 @@ package com.maproom.api.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @Getter @Setter
 public class Room {
-    private int x;
-    private int y;
+    private int longitude;
+    private int latitude;
     private String roomName;
     private String endTime;
+    @Id
+    private String roomKey;
 
-//    private String roomKey;
-
-    public Room(int x, int y, String roomName, String endTime) {
-        this.x = x;
-        this.y = y;
+    public Room(int longitude, int latitude, String roomName, String endTime) {
+        this.longitude = longitude;
+        this.latitude = latitude;
         this.roomName = roomName;
         this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[roomKey=%s, roomName='%s']", roomKey, roomName
+        );
     }
 }
--- src/main/java/com/maproom/api/repository/MongoDBTestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maproom.api.repository;
-
-import com.maproom.api.domain.MongoDBTestModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface MongoDBTestRepository extends MongoRepository<MongoDBTestModel, String> {
-    MongoDBTestModel findByName(String name);
-}
